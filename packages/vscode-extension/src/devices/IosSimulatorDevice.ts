Apple0717/radon

commit f77bc17c71b896453e00fb5e2f02f0b980a0940c

Migrate cashes directories (#562)
This PR changes the location of caches directory for all new devices,
old devices remain in old cache location and can still be used and
removed by the user by the user.

packages/vscode-extension/src/devices/IosSimulatorDevice.ts
@@ -1,4 +1,4 @@
-import { getAppCachesDir } from "../utilities/common";
+import { getAppCachesDir, getOldAppCachesDir } from "../utilities/common";
 import { DeviceBase } from "./DeviceBase";
 import { Preview } from "./preview";
 import { Logger } from "../Logger";
@@ -60,18 +60,24 @@ export class IosSimulatorDevice extends DeviceBase {
   }
 
   get lockFilePath(): string {
-    const deviceSetLocation = getDeviceSetLocation();
+    const deviceSetLocation = getDeviceSetLocation(this.deviceUDID);
     const pidFile = path.join(deviceSetLocation, this.deviceUDID, "lock.pid");
     return pidFile;
   }
 
   public dispose() {
     super.dispose();
-    return exec("xcrun", ["simctl", "--set", getOrCreateDeviceSet(), "shutdown", this.deviceUDID]);
+    return exec("xcrun", [
+      "simctl",
+      "--set",
+      getOrCreateDeviceSet(this.deviceUDID),
+      "shutdown",
+      this.deviceUDID,
+    ]);
   }
 
   async bootDevice() {
-    const deviceSetLocation = getOrCreateDeviceSet();
+    const deviceSetLocation = getOrCreateDeviceSet(this.deviceUDID);
     try {
       await exec("xcrun", ["simctl", "--set", deviceSetLocation, "boot", this.deviceUDID], {
         allowNonZeroExit: true,
@@ -87,7 +93,7 @@ export class IosSimulatorDevice extends DeviceBase {
   }
 
   async changeSettings(settings: DeviceSettings) {
-    const deviceSetLocation = getOrCreateDeviceSet();
+    const deviceSetLocation = getOrCreateDeviceSet(this.deviceUDID);
     await exec("xcrun", [
       "simctl",
       "--set",
@@ -149,7 +155,7 @@ export class IosSimulatorDevice extends DeviceBase {
     ]);
   }
   async sendBiometricAuthorization(isMatch: boolean) {
-    const deviceSetLocation = getOrCreateDeviceSet();
+    const deviceSetLocation = getOrCreateDeviceSet(this.deviceUDID);
     await exec("xcrun", [
       "simctl",
       "--set",
@@ -165,7 +171,7 @@ export class IosSimulatorDevice extends DeviceBase {
   }
 
   async configureMetroPort(bundleID: string, metroPort: number) {
-    const deviceSetLocation = getOrCreateDeviceSet();
+    const deviceSetLocation = getOrCreateDeviceSet(this.deviceUDID);
     const { stdout: appDataLocation } = await exec("xcrun", [
       "simctl",
       "--set",
@@ -201,7 +207,7 @@ export class IosSimulatorDevice extends DeviceBase {
   }
 
   async launchWithBuild(build: IOSBuildResult) {
-    const deviceSetLocation = getOrCreateDeviceSet();
+    const deviceSetLocation = getOrCreateDeviceSet(this.deviceUDID);
     await exec("xcrun", [
       "simctl",
       "--set",
@@ -218,7 +224,7 @@ export class IosSimulatorDevice extends DeviceBase {
     // 1. Add the deeplink to the scheme approval list
     // 2. Terminate the app if it's running
     // 3. Open the deeplink
-    const deviceSetLocation = getOrCreateDeviceSet();
+    const deviceSetLocation = getOrCreateDeviceSet(this.deviceUDID);
 
     // Add the deeplink to the scheme approval list:
     const schema = new URL(expoDeeplink).protocol.slice(0, -1);
@@ -279,7 +285,7 @@ export class IosSimulatorDevice extends DeviceBase {
     if (build.platform !== DevicePlatform.IOS) {
       throw new Error("Invalid platform");
     }
-    const deviceSetLocation = getOrCreateDeviceSet();
+    const deviceSetLocation = getOrCreateDeviceSet(this.deviceUDID);
     if (forceReinstall) {
       try {
         await exec(
@@ -309,7 +315,7 @@ export class IosSimulatorDevice extends DeviceBase {
     await exec("xcrun", [
       "simctl",
       "--set",
-      getOrCreateDeviceSet(),
+      getOrCreateDeviceSet(this.deviceUDID),
       "privacy",
       this.deviceUDID,
       "reset",
@@ -320,7 +326,7 @@ export class IosSimulatorDevice extends DeviceBase {
   }
 
   makePreview(): Preview {
-    return new Preview(["ios", "--id", this.deviceUDID, "--device-set", getOrCreateDeviceSet()]);
+    return new Preview(["ios", "--id", this.deviceUDID, "--device-set", getOrCreateDeviceSet(this.deviceUDID)]);
   }
 }
 
@@ -348,28 +354,55 @@ export async function removeIosSimulator(udid: string | undefined, location: Sim
 
   let deviceSetArgs: string[] = [];
   if (location === SimulatorDeviceSet.RN_IDE) {
-    const setDirectory = getOrCreateDeviceSet();
+    const setDirectory = getOrCreateDeviceSet(udid);
     deviceSetArgs = ["--set", setDirectory];
   }
 
   return exec("xcrun", ["simctl", ...deviceSetArgs, "delete", udid]);
 }
 
+async function listSimulatorsForLocation(location?: string) {
+  let deviceSetArgs: string[] = [];
+  if (location) {
+    deviceSetArgs = ["--set", location];
+  }
+  try {
+    const { stdout } = await exec(
+      "xcrun",
+      ["simctl", ...deviceSetArgs, "list", "devices", "--json"],
+      { allowNonZeroExit: true }
+    );
+    const parsedData: SimulatorData = JSON.parse(stdout);
+
+    const { devices: devicesPerRuntime } = parsedData;
+
+    return Object.entries(devicesPerRuntime);
+  } catch (e) {
+    // ignore errors because some locations might not exist
+  }
+  return [];
+}
+
 export async function listSimulators(
   location: SimulatorDeviceSet = SimulatorDeviceSet.RN_IDE
 ): Promise<IOSDeviceInfo[]> {
-  let deviceSetArgs: string[] = [];
+  let devicesPerRuntime;
   if (location === SimulatorDeviceSet.RN_IDE) {
     const deviceSetLocation = getOrCreateDeviceSet();
-    deviceSetArgs = ["--set", deviceSetLocation];
-  }
-  const { stdout } = await exec("xcrun", ["simctl", ...deviceSetArgs, "list", "devices", "--json"]);
-  const parsedData: SimulatorData = JSON.parse(stdout);
 
-  const { devices: devicesPerRuntime } = parsedData;
+    devicesPerRuntime = await listSimulatorsForLocation(deviceSetLocation);
+
+    const oldDeviceSetLocation = getOldDeviceSetLocation();
+    const oldDevicesPerRuntime = await listSimulatorsForLocation(oldDeviceSetLocation);
+
+    devicesPerRuntime = devicesPerRuntime.concat(oldDevicesPerRuntime);
+  } else {
+    devicesPerRuntime = await listSimulatorsForLocation();
+  }
+
   const runtimes = await getAvailableIosRuntimes();
 
-  const simulators = Object.entries(devicesPerRuntime)
+  const simulators = devicesPerRuntime
     .map(([runtimeID, devices]) => {
       const runtime = runtimes.find((item) => item.identifier === runtimeID);
 
@@ -432,13 +465,30 @@ export async function createSimulator(
   } as IOSDeviceInfo;
 }
 
-function getDeviceSetLocation() {
+function getDeviceSetLocation(deviceUDID?: string) {
   const appCachesDir = getAppCachesDir();
-  return path.join(appCachesDir, "Devices", "iOS");
+  const deviceSetLocation = path.join(appCachesDir, "Devices", "iOS");
+  if (!deviceUDID) {
+    return deviceSetLocation;
+  }
+  const oldDeviceSetLocation = getOldDeviceSetLocation();
+  if (!fs.existsSync(oldDeviceSetLocation)) {
+    return deviceSetLocation;
+  }
+  const devices = fs.readdirSync(oldDeviceSetLocation);
+  if (devices.includes(deviceUDID)) {
+    return oldDeviceSetLocation;
+  }
+  return deviceSetLocation;
 }
 
-function getOrCreateDeviceSet() {
-  const deviceSetLocation = getDeviceSetLocation();
+function getOldDeviceSetLocation() {
+  const oldAppCachesDir = getOldAppCachesDir();
+  return path.join(oldAppCachesDir, "Devices", "iOS");
+}
+
+function getOrCreateDeviceSet(deviceUDID?: string) {
+  let deviceSetLocation = getDeviceSetLocation(deviceUDID);
   if (!fs.existsSync(deviceSetLocation)) {
     fs.mkdirSync(deviceSetLocation, { recursive: true });
   }

packages/vscode-extension/src/devices/AndroidEmulatorDevice.ts
@@ -5,7 +5,7 @@ import fs from "fs";
 import { EOL } from "node:os";
 import xml2js from "xml2js";
 import { retry } from "../utilities/retry";
-import { getAppCachesDir, getNativeABI } from "../utilities/common";
+import { getAppCachesDir, getNativeABI, getOldAppCachesDir } from "../utilities/common";
 import { ANDROID_HOME } from "../utilities/android";
 import { ChildProcess, exec, lineReader } from "../utilities/subprocess";
 import { v4 as uuidv4 } from "uuid";
@@ -123,7 +123,7 @@ export class AndroidEmulatorDevice extends DeviceBase {
     // this prevents booting device with the same AVD twice
     await ensureOldEmulatorProcessExited(this.avdId);
 
-    const avdDirectory = getOrCreateAvdDirectory();
+    const avdDirectory = getOrCreateAvdDirectory(this.avdId);
     const subprocess = exec(
       EMULATOR_BINARY,
       [
@@ -454,6 +454,16 @@ async function getAvdIds(avdDirectory: string) {
 
 export async function listEmulators() {
   const avdDirectory = getOrCreateAvdDirectory();
+  const emulators = listEmulatorsForDirectory(avdDirectory);
+  const oldAvdDirectory = getOldAvdDirectoryLocation();
+  const oldEmulators = listEmulatorsForDirectory(oldAvdDirectory);
+
+  const combinedEmulators = await Promise.all([emulators, oldEmulators]);
+
+  return combinedEmulators[0].concat(combinedEmulators[1]);
+}
+
+async function listEmulatorsForDirectory(avdDirectory: string) {
   const avdIds = await getAvdIds(avdDirectory);
   const systemImages = await getAndroidSystemImages();
   return Promise.all(
@@ -504,7 +514,7 @@ export async function removeEmulator(avdId: string) {
     await ensureOldEmulatorProcessExited(avdId);
   }
 
-  const avdDirectory = getOrCreateAvdDirectory();
+  const avdDirectory = getOrCreateAvdDirectory(avdId);
   const removeAvd = fs.promises.rm(path.join(avdDirectory, `${avdId}.avd`), {
     recursive: true,
   });
@@ -584,8 +594,8 @@ async function waitForEmulatorOnline(serial: string, timeoutMs: number) {
   ]);
 }
 
-function getOrCreateAvdDirectory() {
-  const avdDirectory = getAvdDirectoryLocation();
+function getOrCreateAvdDirectory(avd?: string) {
+  const avdDirectory = getAvdDirectoryLocation(avd);
   if (!fs.existsSync(avdDirectory)) {
     fs.mkdirSync(avdDirectory, { recursive: true });
   }
@@ -593,9 +603,26 @@ function getOrCreateAvdDirectory() {
   return avdDirectory;
 }
 
-function getAvdDirectoryLocation() {
+function getOldAvdDirectoryLocation() {
+  const oldAppCachesDir = getOldAppCachesDir();
+  return path.join(oldAppCachesDir, "Devices", "Android", "avd");
+}
+
+function getAvdDirectoryLocation(avd?: string) {
   const appCachesDir = getAppCachesDir();
   const avdDirectory = path.join(appCachesDir, "Devices", "Android", "avd");
+  if (!avd) {
+    return avdDirectory;
+  }
+
+  const oldAvdDirectory = getOldAvdDirectoryLocation();
+  if (!fs.existsSync(oldAvdDirectory)) {
+    return avdDirectory;
+  }
+  const devices = fs.readdirSync(oldAvdDirectory);
+  if (devices.includes(`${avd}.avd`)) {
+    return oldAvdDirectory;
+  }
   return avdDirectory;
 }
 

packages/vscode-extension/src/utilities/common.ts
@@ -57,9 +57,13 @@ export function getNativeABI() {
   }
 }
 
+export function getOldAppCachesDir() {
+  return join(os.homedir(), "Library", "Caches", "com.swmansion.react-native-ide");
+}
+
 export function getAppCachesDir() {
   // this one is tricky to rename as Android emulators keep absolute path in config files
-  return join(os.homedir(), "Library", "Caches", "com.swmansion.react-native-ide");
+  return join(os.homedir(), "Library", "Caches", "com.swmansion.radon-ide");
 }
 
 export function getLogsDir() {

commit 80feeee825a617079aec8b6e62bb4385830c480d

Fix new caches location in localization settings (#565)
This PR fixes a bug introduced by #562 witch always searched for
localization settings in new caches directories for ios devices.

Co-authored-by: Filip <filip@Filip-MacBook-Pro.local>

packages/vscode-extension/src/devices/IosSimulatorDevice.ts
@@ -208,7 +208,7 @@ export class IosSimulatorDevice extends DeviceBase {
   }
 
   private async changeLocale(newLocale: Locale): Promise<boolean> {
-    const deviceSetLocation = getOrCreateDeviceSet();
+    const deviceSetLocation = getOrCreateDeviceSet(this.deviceUDID);
     const languageCode = newLocale.match(/([^_-]*)/)![1];
     await exec("/usr/libexec/PlistBuddy", [
       "-c",