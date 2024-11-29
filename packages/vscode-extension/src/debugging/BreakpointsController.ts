Apple0717/radon

commit 6194d698d5bc3dacaf52fdcc1bfd5ca1a8159fce

Refactor debug adapter (#755)
This PR refactors debug adapter to handle two issues: 
(1) - the `debugAdapter.ts` - file was becoming hard to manage due to
it's versatility of use
(2) - solve problems with multiple sources triggering breakpoint updates
at the same time

to solve (1) we separate out: `CDPSession`, `SourceMapController` and
`breakpointsController`
to solve (2) we do the following:

a) move the responsibility of updating the breakpoints from
`breakpointsController` to `CDPBreakpoint` class itself and queue CDP
communication associated with that breakpoint such that we never request
to set breakpoint with CDP until we get a response from previous
breakpoint
b) we clear breakpoints registry when execution context is clear – we
will get a new set of breakpoints once the new context is initialized
c) we only send InitializedRequest for DAP once main script is parsed.
This triggers DAP to set breakpoints. Without this, we'd get breakpoints
requests soon enough such that we wouldn't have the source map yet and
hence we wouldn't be able to set them with CDP
d) we update logic for determining websocket URL for expo go – now we
expect two runtimes to be present with expo go with the new debugger.


### How Has This Been Tested: 
- Test the following scenarios related to debugging on both Android and
iOS on various projects listed later:
- Load IDE with breakpoint added – make sure the debugger stops at that
breakpoint
- Remove and add breakpoint while app is running – make sure the
debugger stops at the new and doesn't stop at the removed one
- Reload JS with breakpoint set – make sure breakpoint stops after
reload
- Fast refresh for a file with breakpoint set – make sure the breakpoint
works after reload

This has been tested on different apps on Android and iOS:
- test projects: expo-go, react-native-75, react-native-76 (new
debugger), expo-52 (new debugger)

packages/vscode-extension/src/debugging/BreakpointsController.ts
@@ -1,0 +1,81 @@
+import { Breakpoint } from "@vscode/debugadapter";
+import { DebugProtocol } from "@vscode/debugprotocol";
+import { SourceMapConsumer } from "source-map";
+import { CDPBreakpoint } from "./CDPBreakpoint";
+import { SourceMapsRegistry } from "./SourceMapsRegistry";
+import { CDPSession } from "./CDPSession";
+
+export class BreakpointsController {
+  private breakpoints = new Map<string, Array<CDPBreakpoint>>();
+
+  constructor(private sourceMapController: SourceMapsRegistry, private cdpSession: CDPSession) {}
+
+  public resetBreakpoints() {
+    this.breakpoints.clear();
+  }
+
+  public updateBreakpointsInSource(sourceURL: string, consumer: SourceMapConsumer) {
+    // this method gets called after we are informed that a new script has been parsed. If we
+    // had breakpoints set in that script, we need to let the runtime know about it
+
+    // the number of consumer mapping entries can be close to the number of symbols in the source file.
+    // we optimize the process by collecting unique source URLs which map to actual individual source files.
+    // note: apparently despite the TS types from the source-map library, mapping.source can be null
+    const uniqueSourceMapPaths = new Set<string>();
+    consumer.eachMapping((mapping) => mapping.source && uniqueSourceMapPaths.add(mapping.source));
+
+    uniqueSourceMapPaths.forEach((sourceMapPath) => {
+      const absoluteFilePath =
+        this.sourceMapController.toAbsoluteFilePathFromSourceMapAlias(sourceMapPath);
+      const breakpoints = this.breakpoints.get(absoluteFilePath) || [];
+      breakpoints.forEach(async (bp) => {
+        await bp.reset();
+      });
+    });
+  }
+
+  public async setBreakpoints(
+    sourcePath: string,
+    breakpoints: DebugProtocol.SourceBreakpoint[] | undefined
+  ) {
+    const previousBreakpoints = this.breakpoints.get(sourcePath) || [];
+
+    const newBreakpoints = (breakpoints || []).map((bp) => {
+      const previousBp = previousBreakpoints.find(
+        (prevBp) => prevBp.line === bp.line && prevBp.column === bp.column
+      );
+      if (previousBp) {
+        return previousBp;
+      } else {
+        return new CDPBreakpoint(
+          this.cdpSession,
+          this.sourceMapController,
+          sourcePath,
+          bp.line,
+          bp.column
+        );
+      }
+    });
+
+    // remove old breakpoints
+    previousBreakpoints.forEach((bp) => {
+      if (
+        bp.verified &&
+        !newBreakpoints.find((newBp) => newBp.line === bp.line && newBp.column === bp.column)
+      ) {
+        bp.delete();
+      }
+    });
+
+    this.breakpoints.set(sourcePath, newBreakpoints);
+
+    const resolvedBreakpoints = await Promise.all<Breakpoint>(
+      newBreakpoints.map(async (bp) => {
+        await bp.set();
+        return bp;
+      })
+    );
+
+    return resolvedBreakpoints;
+  }
+}

packages/vscode-extension/src/debugging/CDPBreakpoint.ts
@@ -1,0 +1,98 @@
+import { Breakpoint } from "@vscode/debugadapter";
+import { SourceMapsRegistry } from "./SourceMapsRegistry";
+import { CDPSession } from "./CDPSession";
+
+function makeDeferredTaskPromise<T>(task: () => Promise<T>) {
+  let resolve: (value: T | PromiseLike<T>) => void;
+  let reject: (reason?: any) => void;
+  const promise = new Promise<T>((res, rej) => {
+    resolve = res;
+    reject = rej;
+  });
+  const runTask = () => {
+    task().then(resolve, reject);
+  };
+  return { promise, runTask };
+}
+
+let breakpointId = 0;
+
+export class CDPBreakpoint extends Breakpoint {
+  private cdpId: number | undefined;
+  private lastTaskPromise = Promise.resolve();
+
+  constructor(
+    private cdpSession: CDPSession,
+    private sourceMapController: SourceMapsRegistry,
+    private sourcePath: string,
+    public readonly line: number,
+    public readonly column: number | undefined
+  ) {
+    super(false, line, column);
+  }
+
+  public delete() {
+    return this.scheduleTask(this.deleteWithCDP);
+  }
+
+  public async reset() {
+    this.delete();
+    return this.set();
+  }
+
+  public set() {
+    return this.scheduleTask(this.setWithCDP);
+  }
+
+  private setWithCDP = async () => {
+    if (this.verified) {
+      return;
+    }
+
+    const generatedPos = this.sourceMapController.toGeneratedPosition(
+      this.sourcePath,
+      this.line,
+      this.column ?? 0
+    );
+    if (!generatedPos) {
+      return;
+    }
+
+    const result = await this.cdpSession.sendCDPMessage("Debugger.setBreakpointByUrl", {
+      // in CDP line and column numbers are 0-based
+      lineNumber: generatedPos.lineNumber1Based - 1,
+      url: generatedPos.source,
+      columnNumber: generatedPos.columnNumber0Based,
+      condition: "",
+    });
+    if (result && result.breakpointId !== undefined) {
+      this.cdpId = result.breakpointId;
+      this.verified = true;
+      this.setId(breakpointId++);
+    } else {
+      this.verified = false;
+    }
+  };
+
+  private deleteWithCDP = async () => {
+    await this.cdpSession.sendCDPMessage("Debugger.removeBreakpoint", {
+      breakpointId: this.cdpId,
+    });
+    this.verified = false;
+  };
+
+  private async scheduleTask(task: () => Promise<void>) {
+    const { promise, runTask } = makeDeferredTaskPromise(task);
+    if (this.lastTaskPromise) {
+      this.lastTaskPromise
+        .catch(() => {}) // no-op catch hereo allow the next task to run
+        .then(() => {
+          setTimeout(runTask, 0);
+        });
+    } else {
+      runTask();
+    }
+    this.lastTaskPromise = promise;
+    return promise;
+  }
+}

packages/vscode-extension/src/debugging/CDPSession.ts
@@ -1,0 +1,84 @@
+import WebSocket from "ws";
+import { Logger } from "../Logger";
+
+type ResolveType<T = unknown> = (result: T) => void;
+type RejectType = (error: unknown) => void;
+
+type PromiseHandlers<T = unknown> = {
+  resolve: ResolveType<T>;
+  reject: RejectType;
+};
+
+export class CDPSession {
+  private connection: WebSocket;
+
+  private cdpMessageId = 0;
+  private cdpMessagePromises: Map<number, PromiseHandlers> = new Map();
+
+  constructor(
+    websocketAddress: string,
+    onConnectionClosed: () => void,
+    onIncomingCDPMethod: (message: any) => Promise<void>
+  ) {
+    this.connection = new WebSocket(websocketAddress);
+
+    this.connection.on("open", this.setUpDebugger);
+
+    this.connection.on("close", onConnectionClosed);
+
+    this.connection.on("message", async (data) => {
+      const message = JSON.parse(data.toString());
+      if (message.result || message.error) {
+        this.handleCDPMessageResponse(message);
+        return;
+      }
+      await onIncomingCDPMethod(message);
+    });
+  }
+
+  private setUpDebugger = async () => {
+    // the below catch handler is used to ignore errors coming from non critical CDP messages we
+    // expect in some setups to fail
+    const ignoreError = () => {};
+    await this.sendCDPMessage("FuseboxClient.setClientMetadata", {}).catch(ignoreError);
+    await this.sendCDPMessage("Runtime.enable", {});
+    await this.sendCDPMessage("Debugger.enable", { maxScriptsCacheSize: 100000000 });
+    await this.sendCDPMessage("Debugger.setPauseOnExceptions", { state: "none" });
+    await this.sendCDPMessage("Debugger.setAsyncCallStackDepth", { maxDepth: 32 }).catch(
+      ignoreError
+    );
+    await this.sendCDPMessage("Debugger.setBlackboxPatterns", { patterns: [] }).catch(ignoreError);
+    await this.sendCDPMessage("Runtime.runIfWaitingForDebugger", {}).catch(ignoreError);
+  };
+
+  private handleCDPMessageResponse(message: any) {
+    const messagePromise = this.cdpMessagePromises.get(message.id);
+    this.cdpMessagePromises.delete(message.id);
+    if (message.result && messagePromise?.resolve) {
+      messagePromise.resolve(message.result);
+    } else if (message.error && messagePromise?.reject) {
+      Logger.warn("CDP message error received", message.error);
+      // create an error object such that we can capture stack trace and assign
+      // all object error properties as provided by CDP
+      const error = new Error();
+      Object.assign(error, message.error);
+      messagePromise.reject(error);
+    }
+  }
+
+  public closeConnection() {
+    this.connection.close();
+  }
+
+  public async sendCDPMessage(method: string, params: object) {
+    const message = {
+      id: ++this.cdpMessageId,
+      method: method,
+      params: params,
+    };
+    this.connection.send(JSON.stringify(message));
+    return new Promise<any>((resolve, reject) => {
+      this.cdpMessagePromises.set(message.id, { resolve, reject });
+    });
+  }
+}

packages/vscode-extension/src/debugging/SourceMapsRegistry.ts
@@ -1,0 +1,164 @@
+import path from "path";
+import { NullablePosition, SourceMapConsumer } from "source-map";
+import { Logger } from "../Logger";
+
+function compareIgnoringHost(url1: string, url2: string) {
+  try {
+    const firstURL = new URL(url1);
+    const secondURL = new URL(url2);
+    firstURL.hostname = secondURL.hostname = "localhost";
+    firstURL.port = secondURL.port = "8080";
+    return firstURL.href === secondURL.href;
+  } catch (e) {
+    return false;
+  }
+}
+
+export class SourceMapsRegistry {
+  private sourceMaps: Array<[string, string, SourceMapConsumer, number]> = [];
+  private sourceMapFilePaths: Set<string> = new Set();
+
+  constructor(
+    private expoPreludeLineCount: number,
+    private sourceMapAliases?: Array<[string, string]>
+  ) {}
+
+  public clearSourceMaps() {
+    this.sourceMaps = [];
+    this.sourceMapFilePaths.clear();
+  }
+
+  public async registerSourceMap(
+    sourceMap: any,
+    sourceURL: string,
+    scriptId: string,
+    isMainBundle: boolean
+  ): Promise<SourceMapConsumer> {
+    const consumer = await new SourceMapConsumer(sourceMap);
+
+    // Expo env plugin has a bug that causes the bundle to include so-called expo prelude module named __env__
+    // which is not present in the source map. As a result, the line numbers are shifted by the amount of lines
+    // the __env__ module adds. If we detect that main bundle is loaded, but __env__ is not there, we use the provided
+    // expoPreludeLineCount which reflects the number of lines in __env__ module to offset the line numbers in the source map.
+    const bundleContainsExpoPrelude = sourceMap.sources.includes("__env__");
+    let lineOffset = 0;
+    if (isMainBundle && !bundleContainsExpoPrelude && this.expoPreludeLineCount > 0) {
+      Logger.debug(
+        "Expo prelude lines were detected and an offset was set to:",
+        this.expoPreludeLineCount
+      );
+      lineOffset = this.expoPreludeLineCount;
+    }
+
+    // add all sources from consumer to sourceMapFilePaths
+    consumer.sources.forEach((source) => {
+      this.sourceMapFilePaths.add(source);
+    });
+    this.sourceMaps.push([sourceURL, scriptId, consumer, lineOffset]);
+    return consumer;
+  }
+
+  public findOriginalPosition(
+    scriptIdOrURL: string,
+    lineNumber1Based: number,
+    columnNumber0Based: number
+  ) {
+    let scriptURL = "__script__";
+    let sourceURL = "__source__";
+    let sourceLine1Based = lineNumber1Based;
+    let sourceColumn0Based = columnNumber0Based;
+
+    this.sourceMaps.forEach(([url, id, consumer, lineOffset]) => {
+      // when we identify script by its URL we need to deal with a situation when the URL is sent with a different
+      // hostname and port than the one we have registered in the source maps. The reason for that is that the request
+      // that populates the source map (scriptParsed) is sent by metro, while the requests from breakpoints or logs
+      // are sent directly from the device. In different setups, specifically on Android emulator, the device uses different URLs
+      // than localhost because it has a virtual network interface. Hence we need to unify the URL:
+      if (id === scriptIdOrURL || compareIgnoringHost(url, scriptIdOrURL)) {
+        scriptURL = url;
+        const pos = consumer.originalPositionFor({
+          line: lineNumber1Based - lineOffset,
+          column: columnNumber0Based,
+        });
+        if (pos.source !== null) {
+          sourceURL = pos.source;
+        }
+        if (pos.line !== null) {
+          sourceLine1Based = pos.line;
+        }
+        if (pos.column !== null) {
+          sourceColumn0Based = pos.column;
+        }
+      }
+    });
+
+    return {
+      sourceURL: this.toAbsoluteFilePathFromSourceMapAlias(sourceURL),
+      lineNumber1Based: sourceLine1Based,
+      columnNumber0Based: sourceColumn0Based,
+      scriptURL,
+    };
+  }
+
+  public toGeneratedPosition(
+    absoluteFilePath: string,
+    lineNumber1Based: number,
+    columnNumber0Based: number
+  ) {
+    // New React Native 76 debugger uses file aliases in source maps, however, the aliases are not
+    // used in some settings (i.e. with Expo projects). For calculating the generated position, we
+    // need to use the file path that is present in source maps. We first try to check if the aliased
+    // file path is there, and if it's not, we use the original absolute file path.
+    let sourceMapAliasedFilePath = this.toSourceMapAliasedFilePath(absoluteFilePath);
+    let sourceMapFilePath = this.sourceMapFilePaths.has(sourceMapAliasedFilePath)
+      ? sourceMapAliasedFilePath
+      : absoluteFilePath;
+
+    let position: NullablePosition = { line: null, column: null, lastColumn: null };
+    let originalSourceURL: string = "";
+    this.sourceMaps.forEach(([sourceURL, scriptId, consumer, lineOffset]) => {
+      const pos = consumer.generatedPositionFor({
+        source: sourceMapFilePath,
+        line: lineNumber1Based,
+        column: columnNumber0Based,
+        bias: SourceMapConsumer.LEAST_UPPER_BOUND,
+      });
+      if (pos.line !== null) {
+        originalSourceURL = sourceURL;
+        position = { ...pos, line: pos.line + lineOffset };
+      }
+    });
+    if (position.line === null) {
+      return null;
+    }
+    return {
+      source: originalSourceURL,
+      lineNumber1Based: position.line,
+      columnNumber0Based: position.column,
+    };
+  }
+
+  public toAbsoluteFilePathFromSourceMapAlias(sourceMapPath: string) {
+    if (this.sourceMapAliases) {
+      for (const [alias, absoluteFilePath] of this.sourceMapAliases) {
+        if (sourceMapPath.startsWith(alias)) {
+          // URL may contain ".." fragments, so we want to resolve it to a proper absolute file path
+          return path.resolve(path.join(absoluteFilePath, sourceMapPath.slice(alias.length)));
+        }
+      }
+    }
+    return sourceMapPath;
+  }
+
+  private toSourceMapAliasedFilePath(sourceAbsoluteFilePath: string) {
+    if (this.sourceMapAliases) {
+      // we return the first alias from the list
+      for (const [alias, absoluteFilePath] of this.sourceMapAliases) {
+        if (absoluteFilePath.startsWith(absoluteFilePath)) {
+          return path.join(alias, path.relative(absoluteFilePath, sourceAbsoluteFilePath));
+        }
+      }
+    }
+    return sourceAbsoluteFilePath;
+  }
+}

packages/vscode-extension/src/project/metro.ts
@@ -343,25 +343,36 @@ export class Metro implements Disposable {
     return websocketAddress;
   }
 
+  private filterNewDebuggerPages(listJson: CDPTargetDescription[]) {
+    return listJson.filter(
+      (page) => page.reactNative && page.title.startsWith("React Native Bridge")
+    );
+  }
+
   private lookupWsAddressForNewDebugger(listJson: CDPTargetDescription[]) {
-    // in the new debugger, ids are generated in the following format: "deviceId-pageId"
+    // In the new debugger, ids are generated in the following format: "deviceId-pageId"
     // but unlike with the old debugger, deviceId is a hex string (UUID most likely)
     // that is stable between reloads.
     // Subsequent runtimes that register get incremented pageId (e.g. main runtime will
     // be 1, reanimated worklet runtime would get 2, etc.)
     // The most recent runtimes are listed first, so we can pick the first one with title
-    // that that starts with "React Native Bridge" (which is the main runtime)
-    const newDebuggerPages = listJson.filter(
-      (page) => page.reactNative && page.title.startsWith("React Native Bridge")
-    );
+    // that starts with "React Native Bridge" (which is the main runtime)
+    const newDebuggerPages = this.filterNewDebuggerPages(listJson);
     if (newDebuggerPages.length > 0) {
-      // Expo go apps would report at least two pages, first one being the Expo Go host runtime
-      // If we detect Expo Go package (using description field), we want to pick the second page
-      // otherwise we pick the first one
       const description = newDebuggerPages[0].description;
       const isExpoGo = description === EXPO_GO_BUNDLE_ID || description === EXPO_GO_PACKAGE_NAME;
-      if (isExpoGo && newDebuggerPages.length > 1) {
-        return newDebuggerPages[1].webSocketDebuggerUrl;
+      if (isExpoGo) {
+        // Expo go apps using the new debugger would report at least two pages, first one being the Expo Go
+        // host runtime.
+        // If we detect Expo Go package (using description field), we want to pick the second page
+        // otherwise we pick the first one
+        // When only one page is listed, it means that the app runtime hasn't registered yet in which case
+        // we return undefined which triggers a retry mechanism.
+        if (newDebuggerPages.length === 1) {
+          return undefined;
+        } else {
+          return newDebuggerPages[1].webSocketDebuggerUrl;
+        }
       }
       return newDebuggerPages[0].webSocketDebuggerUrl;
     }
@@ -373,16 +384,13 @@ export class Metro implements Disposable {
     const list = await fetch(`http://localhost:${this._port}/json/list`);
     const listJson = await list.json();
 
-    // we try using "new debugger" lookup first, and then switch to trying out
-    // the old debugger connection (we can tell by the page naming scheme whether
-    // it's the old or new debugger)
-    let websocketAddress = this.lookupWsAddressForNewDebugger(listJson);
-    if (websocketAddress) {
-      this.usesNewDebugger = true;
-    } else {
-      this.usesNewDebugger = false;
-      websocketAddress = this.lookupWsAddressForOldDebugger(listJson);
-    }
+    // When there are pages that are identified as belonging to the new debugger, we
+    // assume the use of the new debugger and use new debugger logic to determine the websocket address.
+    this.usesNewDebugger = this.filterNewDebuggerPages(listJson).length > 0;
+
+    let websocketAddress = this.usesNewDebugger
+      ? this.lookupWsAddressForNewDebugger(listJson)
+      : this.lookupWsAddressForOldDebugger(listJson);
 
     if (websocketAddress) {
       // Port and host in webSocketDebuggerUrl are set manually to match current metro address,

commit 01a2606c32566dfeeeb9820af823464351aabfc8

Fix breakpoints set in between JS reloads when using new debugger (#786)
This PR fixes the issue when breakpoints set before JS reloads on the
new debugger would be effectively set twice and we wouldn't have a way
of deleting them later on. This issue is a regression introduced in #755

The issue stems from a difference in behavior between old and new
debugger. The old debugger, when JS is reloaded, looses all the
information about the previously set breakpoints. Thanks to that, we can
forget all the breakpoints in our Debug Adapter and rely on VSCode to
request setting them again when new context is instantiated (we send
InitializedRequest).

On the other hand, the new debugger keeps the breakpoints in between
reloads. Therefore, resetting the list of breakpoints in our registry
would lead to the cases where we'd set a single breakpoint for the
second time with CDP after reload. As a consequence, we wouldn't be able
to delete the breakpoint that was set at the beginning.

This PR adds a new parameter that indicates whether the breakpoints get
cleared on context reload. We only set that flag for the old debugger.

### How Has This Been Tested: 
The below is the scenario that was failing:
1. Open app and set breakpoint
2. Reload JS
3. Unset breakpoint
4. Before this change the VM would still be stopping at the breakpoint
that is no longer set with the new debugger.

This scenario was tested in RN 76 and RN 74 projects.

packages/vscode-extension/src/debugging/BreakpointsController.ts
@@ -8,10 +8,29 @@ import { CDPSession } from "./CDPSession";
 export class BreakpointsController {
   private breakpoints = new Map<string, Array<CDPBreakpoint>>();
 
-  constructor(private sourceMapController: SourceMapsRegistry, private cdpSession: CDPSession) {}
+  constructor(
+    private sourceMapController: SourceMapsRegistry,
+    private cdpSession: CDPSession,
+    private breakpointsAreRemovedOnContextCleared: boolean
+  ) {}
 
-  public resetBreakpoints() {
-    this.breakpoints.clear();
+  public onContextCleared() {
+    // When execution context is cleared (JS reloads), the old RN debugger would forget
+    // all the breakpoints that were previously set. This leads to the situation, when
+    // we may get breakpoint IDs misaligned. Because of that, when we execute CDPBreakpoint.reset
+    // to update the breakpoint, we may attempt to delete a different breakpoint or we'd just fail to
+    // delete it altogehter if that breakpoint hasn't been yet set in the new execution context.
+    // In that scenario, it is better to clear all the breakpints we'd previously set as we'd get a
+    // request from DAP to set them again when the new context is ready (we send InitializeRequest).
+    //
+    // On the other hand, we can't reset the breakpoints with the new debugger. If we do that,
+    // the breakpoints that are set in between reloads will be set twice with CDP. There is no
+    // API in CDP that'd allow us to query for the existing breakpoints, so we rely on the fact
+    // that ths breakpoints are maintained including the IDs that we got when we set them.
+    if (this.breakpointsAreRemovedOnContextCleared) {
+      // we forget all the breakpoints when the context is cleared
+      this.breakpoints.clear();
+    }
   }
 
   public updateBreakpointsInSource(sourceURL: string, consumer: SourceMapConsumer) {

packages/vscode-extension/src/debugging/CDPBreakpoint.ts
@@ -75,9 +75,12 @@ export class CDPBreakpoint extends Breakpoint {
   };
 
   private deleteWithCDP = async () => {
-    await this.cdpSession.sendCDPMessage("Debugger.removeBreakpoint", {
-      breakpointId: this.cdpId,
-    });
+    if (this.cdpId !== undefined && this.verified) {
+      await this.cdpSession.sendCDPMessage("Debugger.removeBreakpoint", {
+        breakpointId: this.cdpId,
+      });
+    }
+    this.cdpId = undefined;
     this.verified = false;
   };
 

packages/vscode-extension/src/debugging/DebugAdapter.ts
@@ -66,7 +66,11 @@ export class DebugAdapter extends DebugSession {
       configuration.sourceMapAliases
     );
 
-    this.breakpointsController = new BreakpointsController(this.sourceMapRegistry, this.cdpSession);
+    this.breakpointsController = new BreakpointsController(
+      this.sourceMapRegistry,
+      this.cdpSession,
+      configuration.breakpointsAreRemovedOnContextCleared
+    );
   }
 
   private handleIncomingCDPMethodCalls = async (message: any) => {
@@ -119,7 +123,7 @@ export class DebugAdapter extends DebugSession {
         const allThreads = this.threads;
         this.threads = [];
         this.sourceMapRegistry.clearSourceMaps();
-        this.breakpointsController.resetBreakpoints();
+        this.breakpointsController.onContextCleared();
         this.variableStore.clearReplVariables();
         this.variableStore.clearCDPVariables();
 

packages/vscode-extension/src/debugging/DebugSession.ts
@@ -47,7 +47,8 @@ export class DebugSession implements Disposable {
     }
 
     let sourceMapAliases: Array<[string, string]> = [];
-    if (this.metro.isUsingNewDebugger && this.metro.watchFolders.length > 0) {
+    const isUsingNewDebugger = this.metro.isUsingNewDebugger;
+    if (isUsingNewDebugger && this.metro.watchFolders.length > 0) {
       // first entry in watchFolders is the project root
       sourceMapAliases.push(["/[metro-project]/", this.metro.watchFolders[0]]);
       this.metro.watchFolders.forEach((watchFolder, index) => {
@@ -64,6 +65,7 @@ export class DebugSession implements Disposable {
         websocketAddress: websocketAddress,
         sourceMapAliases,
         expoPreludeLineCount: this.metro.expoPreludeLineCount,
+        breakpointsAreRemovedOnContextCleared: isUsingNewDebugger ? false : true, // new debugger properly keeps all breakpoints in between JS reloads
       },
       {
         suppressDebugStatusbar: true,